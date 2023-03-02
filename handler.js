0d1fc6c07516cbedf6c4870b5b78365a96592ad1
mogakamo <mogaka.amo254@gmail.com>
2023-03-02 20:13:22 +0300

    [commit]: latest project

--- handler.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import app from './src/index.ts';
-import serverless from 'serverless-http';
-
-export const hello = serverless(app);
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,13 @@
-/**
- * Required External Modules
- */
+import './pre-start'; // Must be the first import
+import logger from 'jet-logger';
 
-import * as dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import { connect } from "./api/utils/prisma";
-import log from "./api/utils/logger";
+import EnvVars from '@src/constants/EnvVars';
+import server from './server';
 
-import router from "./api/v1/routes";
 
-dotenv.config();
+// **** Run **** //
 
-/**
- * App Variables
- */
+const SERVER_START_MSG = ('Express server started on port: ' + 
+  EnvVars.Port.toString());
 
-if (!process.env.PORT) {
-    process.exit(1);
-}
-
-const PORT: number = parseInt(process.env.PORT as string, 10);
-
-const app = express();
-
-/**
- *  App Configuration
- */
-
-app.use("/api/v1", router);
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-/**
- * Server Activation
- */
-
-app.listen(PORT, () => {
-    log.info(`🚀  API is listening on port ${PORT}`);
-    connect()
-});
-
-export default app;
+server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
