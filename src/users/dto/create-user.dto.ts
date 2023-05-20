72515466e9a738398ff55b0c8cc97e5b5c3b8da7
mogakamo <mogaka.amo254@gmail.com>
2023-05-20 09:16:01 +0300

    commit: updated the user property

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,21 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
+  @ApiProperty()
   password: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   role: Role;
 }
 
